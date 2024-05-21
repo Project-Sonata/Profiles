e5bccb35a91e99505f5fa1777cdf4bbcd7dd48b6
odeyalo <theprogrammerinfuture@gmail.com>
2024-05-21 21:51:15 +0300

    Added mapstruct mapper to map the UserProfile to UserProfileDto

--- src/main/java/com/odeyalo/sonata/profiles/api/rest/ProfileController.java
+++ src/main/java/com/odeyalo/sonata/profiles/api/rest/ProfileController.java
@@ -2,6 +2,7 @@ package com.odeyalo.sonata.profiles.api.rest;
 
 import com.odeyalo.sonata.profiles.api.dto.UserProfileDto;
 import com.odeyalo.sonata.profiles.service.ProfileService;
+import com.odeyalo.sonata.profiles.support.mapper.UserProfileDtoMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,24 +14,19 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/users")
 public final class ProfileController {
     private final ProfileService profileService;
+    private final UserProfileDtoMapper userProfileDtoMapper;
 
-    public ProfileController(final ProfileService profileService) {
+    public ProfileController(final ProfileService profileService,
+                             final UserProfileDtoMapper userProfileDtoMapper) {
         this.profileService = profileService;
+        this.userProfileDtoMapper = userProfileDtoMapper;
     }
 
     @GetMapping("/{user_id}")
     public Mono<ResponseEntity<UserProfileDto>> fetchUserProfileById(@PathVariable("user_id") final String userId) {
 
         return profileService.getProfileForUser(userId)
-                .map(it -> UserProfileDto.builder()
-                        .id(userId)
-                        .contextUri(it.getContextUri())
-                        .displayName(it.getDisplayName())
-                        .email(it.getEmail())
-                        .birthdate(it.getBirthdate())
-                        .gender(it.getGender())
-                        .countryCode(it.getCountry())
-                        .build())
+                .map(userProfileDtoMapper::toUserProfileDto)
                 .map(ResponseEntity::ok)
                 .defaultIfEmpty(ResponseEntity.noContent().build());
     }
