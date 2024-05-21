41c9508b7e6cf2010cb0897b0fb044fca0e50c3a
odeyalo <theprogrammerinfuture@gmail.com>
2024-05-21 21:25:09 +0300

    ProfileService now uses UserProfileRepository to fetch the user's profile

--- src/main/java/com/odeyalo/sonata/profiles/service/ProfileService.java
+++ src/main/java/com/odeyalo/sonata/profiles/service/ProfileService.java
@@ -1,27 +1,33 @@
 package com.odeyalo.sonata.profiles.service;
 
-import com.odeyalo.sonata.profiles.model.Gender;
+import com.odeyalo.sonata.profiles.entity.UserProfileEntity;
 import com.odeyalo.sonata.profiles.model.UserProfile;
+import com.odeyalo.sonata.profiles.repository.UserProfileRepository;
 import org.jetbrains.annotations.NotNull;
 import reactor.core.publisher.Mono;
 
-import java.time.LocalDate;
-import java.time.Month;
-
 public final class ProfileService {
+    private final UserProfileRepository profileRepository;
+
+    public ProfileService(final UserProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
+    }
 
     @NotNull
     public Mono<UserProfile> getProfileForUser(final String userId) {
-        return Mono.just(
-                UserProfile.builder()
-                        .id(userId)
-                        .displayName("NakanoMiku")
-                        .country("JP")
-                        .email("mikunakano@gmail.com")
-                        .birthdate(LocalDate.of(2004, Month.MAY, 17))
-                        .contextUri("sonata:user:" + userId)
-                        .gender(Gender.FEMALE)
-                        .build()
-        );
+        return profileRepository.findByPublicId(userId)
+                .map(ProfileService::toUserProfile);
+    }
+
+    private static UserProfile toUserProfile(final UserProfileEntity it) {
+        return UserProfile.builder()
+                .id(it.getPublicId())
+                .displayName(it.getDisplayName())
+                .country(it.getCountry())
+                .email(it.getEmail())
+                .birthdate(it.getBirthdate())
+                .contextUri(it.getContextUri())
+                .gender(it.getGender())
+                .build();
     }
 }
--- src/test/java/com/odeyalo/sonata/profiles/service/ProfileServiceTest.java
+++ src/test/java/com/odeyalo/sonata/profiles/service/ProfileServiceTest.java
@@ -1,12 +1,17 @@
 package com.odeyalo.sonata.profiles.service;
 
+import com.odeyalo.sonata.profiles.entity.UserProfileEntity;
 import com.odeyalo.sonata.profiles.model.UserProfile;
+import com.odeyalo.sonata.profiles.repository.memory.InMemoryUserProfileRepository;
 import org.junit.jupiter.api.Test;
 import reactor.test.StepVerifier;
 import testing.faker.UserProfileFaker;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static com.odeyalo.sonata.profiles.model.Gender.FEMALE;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -131,17 +136,39 @@ class ProfileServiceTest {
     }
 
     static final class TestableBuilder {
+        private final List<UserProfileEntity> predefinedEntities = new ArrayList<>();
 
         public static TestableBuilder instance() {
             return new TestableBuilder();
         }
 
         public TestableBuilder withProfiles(final UserProfile... profiles) {
+
+            final var entities = Arrays.stream(profiles)
+                    .map(TestableBuilder::toUserProfileEntity)
+                    .toList();
+
+            predefinedEntities.addAll(entities);
+
             return this;
         }
 
         public ProfileService build() {
-            return new ProfileService();
+            final InMemoryUserProfileRepository repository =  InMemoryUserProfileRepository.withPredefinedEntities(predefinedEntities);
+            return new ProfileService(repository);
+        }
+
+        // Don't like stuff like this in tests, change it by using Adapter?
+        private static UserProfileEntity toUserProfileEntity(final UserProfile profile) {
+            return UserProfileEntity.builder()
+                    .publicId(profile.getId())
+                    .displayName(profile.getDisplayName())
+                    .email(profile.getEmail())
+                    .birthdate(profile.getBirthdate())
+                    .contextUri(profile.getContextUri())
+                    .country(profile.getCountry())
+                    .gender(profile.getGender())
+                    .build();
         }
     }
 }
