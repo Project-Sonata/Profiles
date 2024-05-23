28db8c4b8d6bef2f72a1a996d6d92cc8a1189416
odeyalo <theprogrammerinfuture@gmail.com>
2024-05-23 18:45:43 +0300

    Added email to CreateUserInfoDto. Email is represented by value object that validates the email

--- src/main/java/com/odeyalo/sonata/profiles/api/dto/CreateUserInfoDto.java
+++ src/main/java/com/odeyalo/sonata/profiles/api/dto/CreateUserInfoDto.java
@@ -1,12 +1,19 @@
 package com.odeyalo.sonata.profiles.api.dto;
 
 import com.odeyalo.sonata.profiles.model.core.Birthdate;
+import com.odeyalo.sonata.profiles.model.core.Email;
 import com.odeyalo.sonata.profiles.model.core.Username;
+import lombok.Builder;
+import lombok.Value;
 import org.jetbrains.annotations.NotNull;
 
-public final class CreateUserInfoDto {
+@Value
+@Builder
+public class CreateUserInfoDto {
     @NotNull
     Username username;
     @NotNull
     Birthdate birthdate;
+    @NotNull
+    Email email;
 }
