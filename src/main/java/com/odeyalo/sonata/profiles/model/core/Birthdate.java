bb504bb1d7561ab16ec9068be2d5b42299f203b9
odeyalo <theprogrammerinfuture@gmail.com>
2024-05-23 20:56:46 +0300

    Changed birthdate validation rules. Now birthdate does not contain business specific rules but just checks that birthdate is not in future

--- src/main/java/com/odeyalo/sonata/profiles/model/core/Birthdate.java
+++ src/main/java/com/odeyalo/sonata/profiles/model/core/Birthdate.java
@@ -6,32 +6,20 @@ import org.jetbrains.annotations.NotNull;
 import java.time.LocalDate;
 
 /**
- * Represent the birthdate of the user.
+ * Wrapper around the birthdate.
  * <p>
- * Rules applied for username:
+ * Rules applied for birthdate:
  * <ul>
  *     <li>Cannot be in the future</li>
- *     <li>Allowed minimal age is 14</li>
- *     <li>Allowed minimal age is 150</li>
  * </ul>
  *
  * @param value - local date that represent the date of birth for the user
  */
 public record Birthdate(@NotNull LocalDate value) {
-    private static final int ALLOWED_MIN_AGE = 14;
-    private static final int ALLOWED_MAX_AGE = 150;
 
     public Birthdate {
         if ( TimeUtil.isFuture(value) ) {
             throw new IllegalArgumentException("Invalid birthdate: Birthdate cannot be in the future.");
-        }
-
-        if ( LocalDate.now().minusYears(ALLOWED_MAX_AGE).isAfter(value) ) {
-            throw new IllegalArgumentException("Invalid birthdate: The provided birthdate exceeds the acceptable age limit of 150 years.");
-        }
-
-        if ( LocalDate.now().minusYears(ALLOWED_MIN_AGE).isBefore(value) ) {
-            throw new IllegalArgumentException("Invalid birthdate: The provided birthdate indicates an age younger than the minimum acceptable age of 14 years.");
         }
     }
 }
--- src/test/java/com/odeyalo/sonata/profiles/model/core/BirthdateTest.java
+++ src/test/java/com/odeyalo/sonata/profiles/model/core/BirthdateTest.java
@@ -16,22 +16,4 @@ class BirthdateTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("Invalid birthdate: Birthdate cannot be in the future.");
     }
-
-    @Test
-    void birthdateThatGreaterThan150YearsIsNotAllowed() {
-        final LocalDate exceedLimitBirthdate = LocalDate.now().minusYears(151);
-
-        assertThatThrownBy(() -> new Birthdate(exceedLimitBirthdate))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Invalid birthdate: The provided birthdate exceeds the acceptable age limit of 150 years.");
-    }
-
-    @Test
-    void birthdateCannotBeLessThan14YearsOld() {
-        final LocalDate young = LocalDate.now().minusYears(12);
-
-        assertThatThrownBy(() -> new Birthdate(young))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Invalid birthdate: The provided birthdate indicates an age younger than the minimum acceptable age of 14 years.");
-    }
 }
